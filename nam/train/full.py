Fix joint dataset output normalization hooks

nam/train/full.py
@@ -20,6 +20,8 @@ from torch.utils.data import DataLoader as _DataLoader
 
 from nam.data import ConcatDataset as _ConcatDataset
 from nam.data import Split as _Split
+from nam.data import apply_joint_dataset_hooks as _apply_joint_dataset_hooks
+from nam.data import get_joint_dataset_hooks as _get_joint_dataset_hooks
 from nam.data import init_dataset as _init_dataset
 from nam.train import lightning_module as _lightning_module
 from nam.util import filter_warnings as _filter_warnings
@@ -215,11 +217,11 @@ def main(
 
     dataset_train = _init_dataset(data_config, _Split.TRAIN)
     dataset_validation = _init_dataset(data_config, _Split.VALIDATION)
-    if dataset_train.sample_rate != dataset_validation.sample_rate:
-        raise RuntimeError(
-            "Train and validation data loaders have different data set sample rates: "
-            f"{dataset_train.sample_rate}, {dataset_validation.sample_rate}"
-        )
+    _apply_joint_dataset_hooks(
+        dataset_train=dataset_train,
+        dataset_validation=dataset_validation,
+        hooks=_get_joint_dataset_hooks(data_config.get("joint", [])),
+    )
     model.net.sample_rate = dataset_train.sample_rate
 
     # Perform handshakes: